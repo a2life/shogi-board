86878a9bd4a0a44741f6a069ba6ae04b0877af7c
adachi <dutch@adachis.info>
2023-03-27 16:21:06 -0400

    shuffle option order in case of masked branch. using 'maskBranch' variable name all the way through.

--- src/components/ShowBranches.tsx
+++ src/components/ShowBranches.tsx
@@ -1,10 +1,19 @@
-export const ShowBranches = (props: { Notes: { note: string, index: number }[], branchingHandler: (e: Event) => void, index: number ,nextMove?:boolean }) => {
+export const ShowBranches = (props: { Notes: { note: string, index: number }[], branchingHandler: (e: Event) => void, index: number ,maskBranch?:boolean }) => {
 
-    const MoveSelect = ()=><option disabled value="default">Move?</option>
+    const MoveSelect = ()=><option disabled value="default">Next Move</option>
+    if(props.maskBranch){ // in case of masked branch, shuffle options order for quiz
+        let currentIndex= props.Notes.length, randomIndex;
+        while (currentIndex !=0){
+            randomIndex=Math.floor(Math.random()*currentIndex);
+            currentIndex--;
+
+            [props.Notes[currentIndex],props.Notes[randomIndex]]=[props.Notes[randomIndex],props.Notes[currentIndex]]
+        }
+    }
     if (props.Notes.length > 1) {
         return (
             <select class="option-select" onChange={props.branchingHandler}
-                value={props.nextMove?"default":props.index}   > {props.nextMove && MoveSelect()} {props.Notes.map((e) => {
+                value={props.maskBranch?"default":props.index}   > {props.maskBranch && MoveSelect()} {props.Notes.map((e) => {
                 return <option value={e.index}>{e.note}</option>
             })}</select>)
     } else {

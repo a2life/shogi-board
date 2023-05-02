76a066adbcd603df901c812b412b5337e827af33
adachi <dutch@adachis.info>
2023-05-02 16:11:45 -0400

    keep vite module strategy to split vendor.js and index.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+// vite.config.js
+import { splitVendorChunkPlugin } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), splitVendorChunkPlugin()],
   build: {
     assetsDir: 'assets/shogi-board'
   }

36019406c4f81f98e7f111b88f8ee973ebceed5c
adachi <dutch@adachis.info>
2023-06-29 21:34:42 -0400

    factored shogidata to records folder from index.html

---
+++ public/assets/records/example4-rawdata.js
@@ -0,0 +1,10 @@
+if (typeof(initialSetup__ts81)==='undefined'){initialSetup__ts81=[]}
+initialSetup__ts81.push(
+    {
+        caption: "using raw data for simple moves",
+        sOnBoard: `21r,24p`,
+        gOnBoard: `12k,13p,22s`,
+        sOnHand: `g`,
+        moves: ["sd11g", "g-0022", "s+2321", "x"]
+    },
+)

a542c001d740231c1ddac4dbad9c2ed2b55e14df
adachi <dutch@adachis.info>
2023-12-18 23:30:33 -0500

    added description

--- src/components/eomNote.ts
+++ src/components/eomNote.ts
@@ -1,13 +1,16 @@
 /**
+
+ */
+
+/**
  * End of Moves note extractor
+ * @param s
  * if end of Moves is true, then check to see if description is available. (中断、投了etc.,)
  * Also if long description enclosed with === === exists, then return the quotd string instead of simple description
  * movement[index] should be the parameter, for instance
  * "x:中断===まで8手で中断==="
  * in case of "C:#"  return "Branch # - not sure if this gives any info, but this is a starter
  */
-
-
 export const endOfMoveComment = (s: string | undefined) => {
     if (typeof (s) === "string") {
         console.log('endofmove string:', s);

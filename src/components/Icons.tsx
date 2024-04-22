c65650642e2b30ffa71f35f56ff8b96f5226a52b
adachi <dutch@adachis.info>
2024-04-22 13:44:20 -0400

    button icons updated to fontawesome-base

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -43,7 +43,7 @@ export const Play = () => (
 
 
 export const Back = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause"
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-pause"
          viewBox="0 0 384 512" transform="rotate(180)">
         <path
             d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>

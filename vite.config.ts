b46646d0efcbe3c139f77ca1243d7a311c1e9494
A2life <dutch@adachis.info>
2025-07-12 12:14:07 -0400

    updated preact. removed chunk module plugin. It now generate monolithic index.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 // vite.config.js
-import { splitVendorChunkPlugin } from 'vite'
 import preact from '@preact/preset-vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), splitVendorChunkPlugin()],
+  plugins: [preact()],
   build: {
     assetsDir: 'assets/shogi-board'
+
   }
 })
